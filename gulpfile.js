d2230220b49b80309a0a018ae6a656907f2114a4
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-23 20:11:23 -0300

    add css minify and default task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
+/* eslint-disable no-sparse-arrays */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const imagemin = require('gulp-imagemin')
 const svgmin = require('gulp-svgmin')
 const uglifyJs = require('gulp-uglify-es').default
+const autoprefixer = require('gulp-autoprefixer')
+const clean = require('gulp-clean')
+const minifyCss = require('gulp-clean-css')
 
 gulp.task('img', () => {
-  const svgOrigin = ['/assets/images/***.svg']
-  const directoryProject = ['/assets/images/***']
-  const directoryBuiLd = '/assets/images/'
+  const svgOrigin = ['src/assets/images/***.svg']
+  const directoryProject = ['src/assets/images/***']
+  const directoryBuiLd = 'src/assets/images/'
 
   gulp.src(svgOrigin).pipe(svgmin()).pipe(gulp.dest(directoryBuiLd))
   return gulp.src(directoryProject).pipe(imagemin()).pipe(gulp.dest(directoryBuiLd))
+})
+gulp.task('clean', () => {
+  const srcClean = ['./src/assets/css/*.map']
+  return gulp.src(srcClean, { read: false }).pipe(clean())
 })
 
 gulp.task('js', () => {
@@ -34,4 +42,44 @@ gulp.task('js', () => {
   gulp.src(srcJs[3]).pipe(uglifyJs()).pipe(gulp.dest(distJs))
   gulp.src(srcJs[4]).pipe(uglifyJs()).pipe(gulp.dest(distJs))
   return gulp.src('./public/scripts/js/allPolify.js').pipe(uglifyJs()).pipe(gulp.dest(distJs))
+})
+
+gulp.task('css', () => {
+  const srcCss = ['src/*.css']
+  const distCssBuild = './dist/'
+  const distCssDev = './src/'
+  return gulp
+    .src(srcCss)
+    .pipe(
+      autoprefixer({
+        overrideBrowserlist: [
+          'last 2 version',
+          'safari 5',
+          'ie6',
+          'ie7',
+          'ie 8',
+          'ie 9',
+          'opera 12.1',
+          'ios 6',
+          'android 4',
+          'last 99 versions',
+          'last 3 versions',
+          '> 1%',
+          ,
+        ],
+      })
+    )
+    .pipe(minifyCss())
+    .pipe(gulp.dest(distCssBuild))
+    .pipe(gulp.dest(distCssDev))
+})
+
+gulp.task('default', () => {
+  const files = [
+    './src/assets/scripts/***',
+    './src/assets/sass/***',
+    './src/*',
+    './src/assets/images/**',
+  ]
+  gulp.watch(files, gulp.series('clean', 'img', 'css', 'js'))
 })
