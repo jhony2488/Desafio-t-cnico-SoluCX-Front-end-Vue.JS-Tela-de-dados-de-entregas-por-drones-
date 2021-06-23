729789496bc9123f77055c654197f69e461f3a7b
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-23 20:11:51 -0300

    reconfig vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 /* eslint-disable no-param-reassign */
 module.exports = {
   devServer: { host: 'localhost', port: '8080' },
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `
-        @import "@/assets/sass/main.scss";
-        `,
-      },
-    },
-  },
 }
