138417385337ac8c835697ca282bf2abb1f9edef
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-23 20:20:36 -0300

    Home screen  created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <HomeTemplate />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import HomeTemplate from '../components/templates/Home.vue'
 
-@Options({
+@Component({
   components: {
-    HelloWorld,
+    HomeTemplate,
   },
 })
 export default class Home extends Vue {}
