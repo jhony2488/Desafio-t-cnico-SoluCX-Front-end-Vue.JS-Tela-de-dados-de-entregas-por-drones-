8abb319ae8f23486549444dc7326e437dc4eb00e
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-23 20:16:01 -0300

    utils form filter formated values inputs functions created

---
+++ src/utils/formFilterFormatedValuesInputs.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+class Formated {
+  formFilterFormatedValuesStringInputs(value: string) {
+    const strFormatedValues = value.trim()
+    return strFormatedValues
+  }
+}
+
+export { Formated }
