48ca6595b82008b0e4c3b891e6cb130fcb862045
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-26 00:16:01 -0300

    structural base of template of page 404 created

---
+++ src/components/templates/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <NotFound />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import NotFound from '../organisms/NotFound.vue'
+
+@Component({
+  name: 'NotFoundTemplate',
+  components: { NotFound },
+})
+export default class InputsFilters extends Vue {}
+</script>
+
+<style lang="scss">
+.footer {
+  width: 100%;
+  background-color: black;
+  height: 90px;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+  padding-left: 8px;
+  padding-right: 8px;
+  margin-top: 40px;
+  p {
+    color: white;
+    font-size: 20px;
+    width: 80%;
+  }
+}
+</style>
