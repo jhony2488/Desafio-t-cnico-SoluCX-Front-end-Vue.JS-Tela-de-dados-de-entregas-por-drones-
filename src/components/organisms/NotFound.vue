ecac1c341a30a3da045d3aff1098d1c0a5fe1bbb
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-26 00:15:06 -0300

    structural base of page 404 created

---
+++ src/components/organisms/NotFound.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="not-found">
+    <div class="not-found-wrapper">
+      <p class="err">Error <span>404</span></p>
+      <p>Page Not Found</p>
+      <router-link to="/">Return Home</router-link>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'NotFound',
+})
+export default class OneDriveContent extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.not-found {
+  width: 100vw;
+  height: 100vh;
+  font-size: 28px;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+  .err {
+    font-size: 56px;
+    font-weight: bold;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    span {
+      color: red;
+    }
+  }
+}
+</style>
