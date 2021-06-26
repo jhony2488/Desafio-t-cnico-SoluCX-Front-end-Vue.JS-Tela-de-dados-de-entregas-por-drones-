2c6a9d2db2a70fc1ebe4c79d9aaea159cbcf5c55
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-26 00:13:57 -0300

    drone profile image component created

---
+++ src/components/atoms/imageDrones.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-avatar variant="info" :src="src" :size="size"></b-avatar>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'TableDrones',
+})
+export default class Inputs extends Vue {
+  msg!: string
+
+  @Prop({ type: String }) readonly src: string | undefined
+
+  @Prop({ type: String }) readonly size: string | undefined
+}
+</script>
+
+<style lang="scss"></style>
