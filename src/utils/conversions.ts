fbd067ffa233ee278643184e832efca08f13495f
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-06-28 21:41:19 -0300

    created conversions functions

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -9,20 +9,6 @@ class Conversions {
   StringInNumber(str: string) {
     return Number(str)
   }
-  VelocityInDroneInNumberInteger(number: number) {
-    const n = number
-    const numero = `${n}`
-    const getNumber = numero.split('.')[0]
-    const getNumberInteger = parseInt(getNumber)
-    return getNumberInteger
-  }
-  VelocityInDroneGetNumberAfterPoint(number: number) {
-    const n = number
-    const numero = `${n}`
-    const getNumber= numero.split('.')[1][0]
-    const getNumberInteger = parseInt(getNumber)
-    return getNumberInteger
-  }
 }
 
 export { Conversions }
